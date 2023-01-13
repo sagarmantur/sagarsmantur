24c0b592e818e52a3036cdb37b4994b98b3430c5
sagarmantur <sagar.pheuna@gmail.com>
2023-01-13 23:29:01 +0530

    Uploading my new project it is web app which tells current weather

---
+++ projects/Weather Web App/script.js
@@ -0,0 +1,37 @@
+let weather = {
+    "apikey": "5bfd07703ac7c7a7efc4b0552a7f1be7",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apikey)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+        const { name } = (data);
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".discription").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search()
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Bengaluru")
